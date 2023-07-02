d39af0acb3d54be51cb1415939d3d29586612d83
vetrivelanarasu <vetrivelanarasu@gmail.com>
2023-07-02 09:47:01 +0530

    completed

--- src/component/Rightnav.jsx
+++ src/component/Rightnav.jsx
@@ -1,12 +1,13 @@
 import './Rightnav.css';
 import React, { useContext, useState } from 'react';
-import { FaEllipsisH } from 'react-icons/fa';
+import { FaBars, FaEllipsisH } from 'react-icons/fa';
 import Image from '../assets/image1.png';
 import AuthContextProvider, { AuthContext } from '../Context/AuthContextProvider';
 
 export default function Rightnav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { isAuth, login, logout } = useContext(AuthContext);
+  const screenWidth = window.innerWidth;
 
   const handleClick = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -15,17 +16,14 @@ export default function Rightnav() {
   const handleMenuItemClick = (action) => {
     switch (action) {
       case 'logout':
-        
         logout();
         console.log('Logged out');
         break;
       case 'login':
-       
         login();
         console.log('Logged in');
         break;
       case 'settings':
-        
         console.log('Settings');
         break;
       default:
@@ -35,23 +33,42 @@ export default function Rightnav() {
     setIsMenuOpen(false);
   };
 
+  const renderMenuItems = () => {
+    return (
+      <ul className="menu">
+        <li onClick={() => handleMenuItemClick('logout')} disabled={isAuth}>
+          Logout
+        </li>
+        <li onClick={() => handleMenuItemClick('login')} disabled={!isAuth}>
+          Login
+        </li>
+        <li onClick={() => handleMenuItemClick('settings')}>Settings</li>
+      </ul>
+    );
+  };
+
   return (
-    <div className='rightnav'>
-      <div className='iconic'>
-        <div className="meatball-icon" onClick={handleClick}>
-          <FaEllipsisH className="meatball-icon" />
-        </div>
-        {isMenuOpen && (
-          <ul className="menu">
-            <li onClick={() => handleMenuItemClick('logout')} disabled={isAuth}>Logout</li>
-            <li onClick={() => handleMenuItemClick('login')} disabled={!isAuth}>Login</li>
-            <li onClick={() => handleMenuItemClick('settings')}>Settings</li>
-          </ul>
-        )}
-      </div>
-      <div className='rightnavimg'>
-        <img src={Image} alt='' />
-      </div>
+    <div className="rightnav">
+      {screenWidth < 600 ? (
+        <>
+          <div className="burger-icon" onClick={handleClick}>
+            <FaBars className="burger-icon" />
+          </div>
+          {isMenuOpen && renderMenuItems()}
+        </>
+      ) : (
+        <>
+          <div className="iconic">
+            <div className="meatball-icon" onClick={handleClick}>
+              <FaEllipsisH className="meatball-icon" />
+            </div>
+            {isMenuOpen && renderMenuItems()}
+          </div>
+          <div className="rightnavimg">
+            <img src={Image} alt="" />
+          </div>
+        </>
+      )}
     </div>
   );
 }
